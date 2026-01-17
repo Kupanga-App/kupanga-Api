73f293d11f0270ac64609416bdd46e7fdb58e044
IrALm <moiseaganze76@gmail.com>
2026-01-17 16:54:38 +0100

    Adaptation du point d'entrée de l'application en fonction de l'architecture modulaire

--- src/main/java/com/kupanga/api/ApiApplication.java
+++ src/main/java/com/kupanga/api/ApiApplication.java
@@ -2,11 +2,21 @@ package com.kupanga.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Point d'entrée de l'application.
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.kupanga.api")
+@EnableJpaRepositories(basePackages = {
+        "com.kupanga.api.utilisateur.repository"
+})
+@EntityScan(basePackages = {
+        "com.kupanga.api.utilisateur.entity",
+        "com.kupanga.api.immobilier.entity",
+        "com.kupanga.api.chat.entity"
+})
 public class ApiApplication {
 
 	public static void main(final String[] args) {
--- src/test/java/com/kupanga/api/ApiApplicationTests.java
+++ src/test/java/com/kupanga/api/ApiApplicationTests.java
@@ -1,10 +1,13 @@
 package com.kupanga.api;
 
+import com.kupanga.api.utilisateur.repository.UtilisateurRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@MockBean(UtilisateurRepository.class)
 @ActiveProfiles("test")
 class ApiApplicationTests {
 
