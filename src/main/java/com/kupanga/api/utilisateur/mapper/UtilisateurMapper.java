606045b198eac25a974467702b7bdbcc6e16054d
IrALm <moiseaganze76@gmail.com>
2026-01-22 01:28:29 +0100

    Fix des mappers

--- src/main/java/com/kupanga/api/utilisateur/mapper/UtilisateurMapper.java
+++ src/main/java/com/kupanga/api/utilisateur/mapper/UtilisateurMapper.java
@@ -4,11 +4,17 @@ import com.kupanga.api.utilisateur.dto.formDTO.UtilisateurFormDTO;
 import com.kupanga.api.utilisateur.dto.readDTO.UtilisateurDTO;
 import com.kupanga.api.utilisateur.entity.Utilisateur;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UtilisateurMapper {
 
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "motDePasse", target = "motDePasse")
+    @Mapping(source = "role", target = "role")
     UtilisateurDTO toDTO(Utilisateur utilisateur);
 
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "role", target = "role")
     Utilisateur toEntity(UtilisateurFormDTO utilisateurFormDTO);
 }
